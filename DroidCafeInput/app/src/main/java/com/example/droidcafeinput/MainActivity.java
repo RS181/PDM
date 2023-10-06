d503840aabafdb5d1c0f8253f46a432f54ca0f6a
RS181 <ruisantos.18181@gmail.com>
2023-10-06 19:45:24 +0100

    cod-lab 4.3

--- DroidCafeInput/app/src/main/java/com/example/droidcafeinput/MainActivity.java
+++ DroidCafeInput/app/src/main/java/com/example/droidcafeinput/MainActivity.java
@@ -1,20 +1,110 @@
 package com.example.droidcafeinput;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
+    // Tag for the intent extra.
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.droidcafeinput.extra.MESSAGE";
+
+    // The order message, displayed in the Toast and sent to the new Activity.
+    private String mOrderMessage;
+
+    /**
+     * Creates the content view, the toolbar, and the floating action button.
+     * <p>
+     * This method is provided in the Basic Activity template.
+     *
+     * @param savedInstanceState Saved instance state bundle.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,
+                        OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+            }
+        });
     }
 
+    /**
+     * Inflates the menu, and adds items to the action bar if it is present.
+     *
+     * @param menu Menu to inflate.
+     * @return Returns true if the menu inflated.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Handles app bar item clicks.
+     *
+     * @param item Item clicked.
+     * @return True if one of the defined items was clicked.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        //save the id of each view
+        final int order_id = R.id.action_order;
+        final int status_id = R.id.action_status;
+        final int favorites_id = R.id.action_favorites;
+        final int contact = R.id.action_contact;
+
+
+        if (item.getItemId() == order_id) {
+//            displayToast(getString(R.string.action_order_message));
+            Intent intent = new Intent(MainActivity.this,OrderActivity.class);
+            intent.putExtra(EXTRA_MESSAGE,mOrderMessage);
+            startActivity(intent);
+
+            return true;
+        } else if (item.getItemId() == status_id) {
+
+            displayToast(getString(R.string.action_status_message));
+            return true;
+        } else if (item.getItemId() == favorites_id) {
+            displayToast(getString(R.string.action_favorites_message));
+            return true;
+        } else if (item.getItemId() == contact) {
+            displayToast(getString(R.string.action_contact_message));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Displays a Toast with the message.
+     *
+     * @param message Message to display.
+     */
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message,
                 Toast.LENGTH_SHORT).show();
@@ -24,26 +114,24 @@ public class MainActivity extends AppCompatActivity {
      * Shows a message that the donut image was clicked.
      */
     public void showDonutOrder(View view) {
-        displayToast(getString(R.string.donut_order_message));
+        mOrderMessage = getString(R.string.donut_order_message);
+        displayToast(mOrderMessage);
     }
 
     /**
      * Shows a message that the ice cream sandwich image was clicked.
      */
     public void showIceCreamOrder(View view) {
-        displayToast(getString(R.string.ice_cream_order_message));
+        mOrderMessage = getString(R.string.ice_cream_order_message);
+        displayToast(mOrderMessage);
     }
 
     /**
      * Shows a message that the froyo image was clicked.
      */
     public void showFroyoOrder(View view) {
-        displayToast(getString(R.string.froyo_order_message));
-    }
-
-    public void onClick(View view) {
-        Intent intent = new Intent(MainActivity.this, OrderActivity.class);
-        startActivity(intent);
+        mOrderMessage = getString(R.string.froyo_order_message);
+        displayToast(mOrderMessage);
     }
 
 }
--- DroidCafeInput/app/src/main/java/com/example/droidcafeinput/OrderActivity.java
+++ DroidCafeInput/app/src/main/java/com/example/droidcafeinput/OrderActivity.java
@@ -3,6 +3,7 @@ package com.example.droidcafeinput;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,39 +11,58 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.content.res.Resources;
-public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class OrderActivity extends AppCompatActivity
+        implements AdapterView.OnItemSelectedListener{
 
+    /**
+     * Sets the content view to activity_order, and gets the intent and its
+     * data. Also creates an array adapter and layout for a spinner.
+     *
+     * @param savedInstanceState Saved instance state bundle.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
-        //Create the spinner
+        // Get the intent and its data.
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.order_textview);
+        textView.setText(message);
 
+        // Create the spinner.
         Spinner spinner = findViewById(R.id.label_spinner);
-        if (spinner != null){
+        if (spinner != null) {
             spinner.setOnItemSelectedListener(this);
         }
 
-        // Create ArrayAdapter using the string array and default spinner layout.
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.labels_array, android.R.layout.simple_spinner_item);
+        // Create an ArrayAdapter using the string array and default spinner
+        // layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.labels_array,
+                android.R.layout.simple_spinner_item);
 
         // Specify the layout to use when the list of choices appears.
         adapter.setDropDownViewResource
                 (android.R.layout.simple_spinner_dropdown_item);
+
         // Apply the adapter to the spinner.
         if (spinner != null) {
             spinner.setAdapter(adapter);
         }
     }
 
-    public void displayToast(String message) {
-        Toast.makeText(getApplicationContext(), message,
-                Toast.LENGTH_SHORT).show();
-    }
+    /**
+     * Checks which radio button was clicked and displays a toast message to
+     * show the choice.
+     *
+     * @param view The radio button view.
+     */
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
@@ -73,18 +93,27 @@ public class OrderActivity extends AppCompatActivity implements AdapterView.OnIt
         }
     }
 
+    /**
+     * Displays the actual message in a toast message.
+     *
+     * @param message Message to display.
+     */
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
 
-    /*<?> is a Java type wildcard, enabling the method to be flexible enough to accept any type of AdapterView as an argument*/
+    // Interface callback for when any spinner item is selected.
     @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+    public void onItemSelected(AdapterView<?> adapterView,
+                               View view, int i, long l) {
         String spinnerLabel = adapterView.getItemAtPosition(i).toString();
         displayToast(spinnerLabel);
     }
 
+    // Interface callback for when no spinner item is selected.
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
-        //Not used in this case
+        // Do nothing.
     }
-
-
 }
